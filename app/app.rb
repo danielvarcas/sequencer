889f73ca6e1d682e3b9ef27b2691c9379025e585
Daniel Varcas <daniel.varcas@gmail.com>
2018-07-07 21:49:05 +0100

    older tests are now expecting nil for some reason

--- app/app.rb
+++ app/app.rb
@@ -2,6 +2,15 @@ require_relative '../test/sequencer_test'
 
 class Sequencer
 	def schedule(tasks)
-		tasks.to_s
+		if tasks.class == String
+			tasks
+		end
+
+		if tasks.class == Hash
+			order = []
+			tasks.each do |task|
+				order.push(task) unless order.include? task
+			end
+		end
 	end
 end
