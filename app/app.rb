94a8df1f61ddf8f652f1a4978ec656578a5c21c5
Daniel Varcas <daniel.varcas@gmail.com>
2018-07-08 10:27:27 +0100

    fixed infinite loop

--- app/app.rb
+++ app/app.rb
@@ -12,7 +12,7 @@ class Sequencer
 			until (@counter == 0) || (@order.length == tasks.length) 
 				tasks.each do |key, value|				
 					if (@order.include? value) || (value == "")
-						@order.push(key) # unless @order.include? key
+						@order.push(key) unless @order.include? key
 						@counter = tasks.length
 					else
 						@counter = @counter - 1
