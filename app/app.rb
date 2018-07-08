2561c74eb7cfffddad2549bd80f080576c180169
Daniel Varcas <daniel.varcas@gmail.com>
2018-07-08 10:11:34 +0100

    added algorithm to Sequencer#schedule. test 4 fails.

--- app/app.rb
+++ app/app.rb
@@ -1,16 +1,27 @@
 require_relative '../test/sequencer_test'
 
 class Sequencer
+	def initialise
+		@order = []
+	end
+
 	def schedule(tasks)
 		if tasks.class == String
 			return tasks
 		end
 
 		if tasks.class == Hash
-			order = []
-			tasks.each do |task|
-				order.push(task) unless order.include? task
+			counter = tasks.length
+			tasks.each do |key, value|
+				until counter == 0
+					if @order.include? value
+						@order.push(key) unless @order.include? key
+					else
+						counter = counter - 1
+					end
+				end
 			end
+			@order
 		end
 	end
 end
--- test/sequencer_test.rb
+++ test/sequencer_test.rb
@@ -9,6 +9,30 @@ class SequencerTest < Minitest::Test
 			"b" => "c", 
 			"c" => ""
 		}
+
+		@hash_long = {
+			"a" => "",
+			"b" => "c",
+			"c" => "f",
+			"d" => "a",
+			"e" => "b",
+			"f" => ""
+		}
+
+		@hash_self_dependent = {
+			"a" => "",
+			"b" => "",
+			"c" => "c"
+		}
+
+		@hash_circular ={
+			"a" => "",
+			"b" => "c",
+			"c" => "f",
+			"d" => "a",
+			"e" => "",
+			"f" => "b"
+		}
 	end
 
 	#1
