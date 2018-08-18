856e3777634df3da82c5883c9936fcd94c1ad7c1
Daniel Varcas <daniel.varcas@gmail.com>
2018-08-18 13:17:40 +0100

    multiple task ordering test passes

--- test/orderer_test.rb
+++ test/orderer_test.rb
@@ -8,13 +8,13 @@ class OrdererTest < Minitest::Test
 		assert_equal ["a"], @orderer.order
 	end
 
-# not yet implemented
-=begin
 	def test_it_orders_multiple_tasks
 		@orderer = Orderer.new(:tasks => "abc")
 		assert_equal ["a","b","c"], @orderer.order
 	end
 
+# not yet implemented
+=begin
 	def test_it_orders_tasks_with_dependencies
 		@orderer = Orderer.new({:tasks => "abc", :dependencies => {"a" => "", "b" => "c", "c" => ""}})
 		assert_equal ["a","c","b"], @orderer.order
